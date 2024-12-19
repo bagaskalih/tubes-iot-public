e2022026df04fd29dd8ef08b430c5b35983dd1f1
bagas <bagaskalihputra.bkp@gmail.com>
2024-12-19 07:49:18 +0700

    fix: main.py

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ def view_data():
     data = response.data
     return render_template('index.html', data=data)
 
-# if __name__ == "__main__":
-#     # Start the Flask server
-#     print("Starting Flask server...")
-#     app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    # Start the Flask server
+    app.run()
